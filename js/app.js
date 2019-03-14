a33ff728c930effafe49a796f31fc1084d38c204
lrg1980 <admin@binweb.net>
2019-03-14 14:52:45 -0300

    Subi animación barra de navegación

---
+++ js/app.js
@@ -0,0 +1,18 @@
+// Cambia de color el fondo al hacer scroll
+$(window).scroll = (e) => {
+     const scroll = window.scrollY;
+     
+     const header = document.querySelector('#navegacion-principal');
+     if (scroll > 300) {
+          header.classList.add('bg-success');
+     } else {
+          header.classList.remove('bg-success');
+     }
+};
+
+// Jquery CountDown
+$(document).ready(function () {
+     $('#fecha').countdown('2019/05/23', function (evento) {
+          $(this).html(evento.strftime('<span>%D</span> Días <span>%H</span> Horas <span>%M</span> Minutos <span>%S</span> Segundos'))
+     });
+});
